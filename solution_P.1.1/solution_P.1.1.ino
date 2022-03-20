1ca2b3c25afd63c7c77c3e07669bd8c7d924779f
SKKU_LKN <raylee02@g.skku.edu>
2022-03-21 00:41:24 +0900

    주석 추가

--- solution_P.1.1/solution_P.1.1.ino
+++ solution_P.1.1/solution_P.1.1.ino
@@ -1,6 +1,15 @@
+/**
+ * 핀 번호, 변수명 등은 자신이 원하는대로 설정 가능합니다.
+ */
 #define LED D13
 #define BUTTON D7
 
+/**
+ * // loop에서 수정한 변수가 계속 유지되기 위해서는, loop() 밖에서 변수를
+ * 정의하는 전역변수를 만들어줘야 합니다.
+ * https://dojang.io/mod/page/view.php?id=684 전역변수에 대한 내용은
+ * 다음 링크를 참조해주세요.
+ */
 bool before = LOW;
 bool led = LOW;
 bool now = LOW;
@@ -15,8 +24,13 @@ void loop()
 {
   before = now;
   now = digitalRead(BUTTON);
+
+  /**
+   * 버튼의 상태를 바꾸기 위해서는
+   * 현재 버튼이 눌림 + 이전 상태는 버튼이 눌리지 않음
+   * 상태여야 합니다.
+   */
   if (now && !before) {
-    while (digitalRead(BUTTON));
     led = !led;
   }
   
